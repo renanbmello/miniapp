ffeba88a4d5010587914f479a3f1a065a97876f1
RenatoSouzaAN <renatosouza.an@gmail.com>
2025-02-09 01:04:51 -0300

    Removed commented code and changed address

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ import { VerifyBlock } from "@/components/Verify";
 import MobileProposalDisplay from "@/components/Home/index";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center pt-4 pr-4 pl-4 gap-y-3">
-      <MobileProposalDisplay userAddress={"0x7a45eE0be5C4BdC938A5F00A2AEF393f46502D26"} />
-      {/* <SignIn />
-      <VerifyBlock />
-      <PayBlock /> */}
-    </main>
-  );
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center pt-4 pr-4 pl-4 gap-y-3">
+            <MobileProposalDisplay
+                userAddress={"0xC52b54356854F80AC8cacde8f7b2449bAFDD13DE"}
+            />
+        </main>
+    );
 }
-
-
